a35afa0ee89c9b2cd3320e13832d170a881b87d0
gleb <gleb22ka@gmail.com>
2024-02-22 22:28:59 +0300

    Hw 30

---
+++ src/main/test/java/test.java
@@ -0,0 +1,27 @@
+import Homework30.ArrayReturnNew;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import Homework30.ArrayChecker;
+
+public class test {
+
+    private ArrayReturnNew ArrayUtils;
+
+    @Test
+    public void testGetArrayAfterLastOne() {
+        int[] arr1 = {1, 2, 1, 2, 2};
+        int[] result1 = {2, 2};
+        Assertions.assertArrayEquals(result1, ArrayUtils.getArrayAfterLastOne(arr1));
+
+        int[] arr2 = {2, 2, 2, 2};
+        Assertions.assertThrows(RuntimeException.class, () -> ArrayUtils.getArrayAfterLastOne(arr2));
+    }
+    private ArrayChecker test2;
+    @Test
+    public void checkArray() {
+        int[] array1 = {1, 2};
+        int[] array2 = {1, 1};
+        int[] array3 = {1, 3};
+        int[] array4 = {1, 2, 2, 1};
+    }
+}
