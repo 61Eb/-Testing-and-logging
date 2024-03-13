e32d721ef331653435cebf0e46509332ec65b257
gleb <gleb22ka@gmail.com>
2024-03-13 18:35:23 +0300

    Hw 30

--- src/main/java/Homework30/ArrayChecker.java
+++ src/main/java/Homework30/ArrayChecker.java
@@ -18,6 +18,5 @@ public class ArrayChecker {
     }
 
     public static void main(String[] args) {
-
     }
 }
--- src/main/java/Homework30/ArrayReturnNew.java
+++ src/main/java/Homework30/ArrayReturnNew.java
@@ -18,7 +18,6 @@ public class ArrayReturnNew {
         return result;
     }
 
-
     public static void main(String[] args) {
 
     }
